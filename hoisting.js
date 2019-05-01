39fa03647745b61914036951b2bf2b8dd5c0ca6f
Giovanny <giovannydiaz98@yahoo.com>
2019-05-01 17:35:43 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -8,17 +8,16 @@ function callMe() {
 }
 
 
-function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
 
+function crazy()
+   thisIsCrazy(){
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
 }
 
+
 function sayMyName() {
-  // fix the code in here:
   var name = "Cricky";
 
   sayMy();
